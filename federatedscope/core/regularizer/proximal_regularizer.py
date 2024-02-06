b5cfacbb82ec7e45c9c01a5d18d7dc03e861c60e
chensy <chensy@v.cluster.local>
2024-02-06 18:11:50 -0500

    test2

--- federatedscope/core/regularizer/proximal_regularizer.py
+++ federatedscope/core/regularizer/proximal_regularizer.py
@@ -25,8 +25,24 @@ class ProximalRegularizer(Module):
 
     def forward(self, ctx, p=2):
         norm = 0.
-        for w_init, w in zip(ctx.weight_init, ctx.model.parameters()):
-            norm += torch.pow(torch.norm(w - w_init, p), p)
+        # for w_init, w in zip(ctx.weight_init, ctx.model.parameters()):
+        #     norm += torch.pow(torch.norm(w - w_init, p), p)
+        for name, param in ctx.model.named_parameters():
+            # print(name)
+            if param.requires_grad:
+                if 'lora_B' in name:
+                    B = param
+                    B_BT = torch.matmul(B, B.transpose(0, 1))
+                    I_B = torch.eye(B.size(0), device=B.device)  # Identity matrix matching B's row size
+                    norm_x = torch.norm(B_BT - I_B, p='fro') ** 2
+                elif 'lora_A' in name:
+                    A = param
+                    AT_A = torch.matmul(A.transpose(0, 1), A)
+                    I_A = torch.eye(A.size(1), device=A.device)  # Identity matrix matching A's column size
+                    norm_x = torch.norm(AT_A - I_A, p='fro') ** 2
+        # for w in ctx.model.parameters():
+                norm += norm_x
+        # print(norm)
         return norm * 1. / float(p)
 
 
